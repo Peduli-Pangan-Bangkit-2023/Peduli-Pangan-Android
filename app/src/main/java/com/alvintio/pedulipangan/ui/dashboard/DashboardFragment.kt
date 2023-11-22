279c4f3997e0e7d4b040160babf46924cc4316d7
Edward Ardian Tayu <edwardtayu4@gmail.com>
2023-11-22 13:37:57 +0700

    Add dashboard layout

--- app/src/main/java/com/alvintio/pedulipangan/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/alvintio/pedulipangan/ui/dashboard/DashboardFragment.kt
@@ -24,10 +24,6 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
